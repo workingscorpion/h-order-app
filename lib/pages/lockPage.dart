edabad43dfe55085a2d65ad77334e8130194fd25
Nortina <nortina@globalorder.co.kr>
2021-03-31 15:02:47 +0900

    fix:

--- lib/pages/lockPage.dart
+++ lib/pages/lockPage.dart
@@ -70,36 +70,57 @@ class _LockPageState extends State<LockPage> {
                       color: Colors.white,
                     ),
                   ),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Positioned(
-                        top: _size / 2 - 10 / 2,
-                        left: _size / 2,
-                        child: Transform.rotate(
-                          angle: _radian * (_startAngle + _hourValue),
-                          alignment: Alignment.centerLeft,
-                          child: AnimatedContainer(
-                            duration: Duration(seconds: 1),
-                            curve: Curves.linear,
-                            height: 10,
-                            width: 100,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5)),
+                  child: Transform.rotate(
+                    angle: _radian * _startAngle,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Positioned(
+                          top: _size / 2,
+                          left: _size / 2,
+                          child: Transform.rotate(
+                            angle: _radian * _hourValue,
+                            alignment: Alignment.centerLeft,
+                            child: AnimatedContainer(
+                              duration: Duration(seconds: 1),
+                              curve: Curves.linear,
+                              height: 10,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5)),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Positioned(
-                        top: _size / 2 - 5 / 2,
-                        left: _size / 2,
-                        child: Transform.rotate(
-                          angle: _radian * (_startAngle + _minuteValue),
-                          alignment: Alignment.centerLeft,
-                          child: Container(
-                            height: 5,
+                        Positioned(
+                          top: _size / 2,
+                          left: _size / 2,
+                          child: Transform.rotate(
+                            angle: _radian * _minuteValue,
+                            alignment: Alignment.centerLeft,
+                            child: Container(
+                              height: 5,
+                              width: 180,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5)),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: _size / 2,
+                          left: _size / 2,
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            curve: Curves.linear,
+                            transform:
+                                Transform.rotate(angle: _radian * _secondValue)
+                                    .transform,
+                            height: 2,
                             width: 180,
                             decoration: BoxDecoration(
                               color: Colors.white,
@@ -108,38 +129,8 @@ class _LockPageState extends State<LockPage> {
                             ),
                           ),
                         ),
-                      ),
-                      Positioned(
-                        top: _size / 2 - 2 / 2,
-                        left: _size / 2,
-                        child: AnimatedContainer(
-                          duration: Duration(seconds: 1),
-                          curve: Curves.linear,
-                          transform: Matrix4.rotationZ(
-                              _radian * (_startAngle + _secondValue)),
-                          height: 2,
-                          width: 180,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(5)),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: _size / 2 - 12 / 2,
-                        left: _size / 2 - 12 / 2,
-                        child: AnimatedContainer(
-                          duration: Duration(seconds: 1),
-                          curve: Curves.linear,
-                          height: 12,
-                          width: 12,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(12)),
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -153,6 +144,9 @@ class _LockPageState extends State<LockPage> {
                     Text('$_hourValue'),
                     Text('$_minuteValue'),
                     Text('$_secondValue'),
+                    Text('${Matrix4.rotationZ(_radian * _secondValue)}'),
+                    Text(
+                        '${Matrix4.rotationX(_radian * _secondValue).dimension}'),
                   ],
                 ),
               ),
@@ -167,7 +161,7 @@ class _LockPageState extends State<LockPage> {
     final now = DateTime.now();
     _text = _dateFormat.format(now);
 
-    _secondValue = now.second / 60;
+    _secondValue = -now.second / 60;
     _minuteValue = (now.minute / 60) + (_secondValue / 60);
     _hourValue = ((now.hour / 12) + (_minuteValue / 60)) % 1;
 
