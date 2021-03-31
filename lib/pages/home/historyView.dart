8f0d349050c83172a448db6f987796634780cead
workingscorpion <workingscorpion@globalorder.co.kr>
2021-04-01 00:20:35 +0900

    feat: history Filter

--- lib/pages/home/historyView.dart
+++ lib/pages/home/historyView.dart
@@ -9,6 +9,9 @@ class HistoryView extends StatefulWidget {
 }
 
 class _HistoryViewState extends State<HistoryView> {
+  FocusNode focusNode;
+  String selectedPopupMenu = 'all';
+
   @override
   void initState() {
     super.initState();
@@ -21,9 +24,65 @@ class _HistoryViewState extends State<HistoryView> {
 
   @override
   Widget build(BuildContext context) => Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [],
+          children: [
+            _popup(),
+          ],
         ),
       );
+
+  _popup() => Row(
+        children: [
+          Spacer(),
+          Container(
+            margin: EdgeInsets.only(right: 5),
+            child: Text(_popupText()),
+          ),
+          PopupMenuButton(
+            child: Icon(CupertinoIcons.ellipsis_circle),
+            padding: EdgeInsets.zero,
+            offset: Offset(-24, 0),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: "all",
+                child: Text('전체'),
+              ),
+              PopupMenuItem(
+                value: "apply",
+                child: Text('신청'),
+              ),
+              PopupMenuItem(
+                value: "receipted",
+                child: Text('접수'),
+              ),
+              PopupMenuItem(
+                value: "done",
+                child: Text('처리완료'),
+              ),
+            ],
+            onSelected: (value) {
+              setState(() {
+                selectedPopupMenu = value;
+              });
+            },
+          ),
+        ],
+      );
+
+  _popupText() {
+    switch (selectedPopupMenu) {
+      case 'all':
+        return '전체';
+      case 'apply':
+        return '신청';
+      case 'receipted':
+        return '접수';
+      case 'done':
+        return '처리완료';
+    }
+  }
 }
