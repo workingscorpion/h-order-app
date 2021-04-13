0122beaa074d8646ef686adf9fa96d8ac7425d75
workingscorpion <workingscorpion@globalorder.co.kr>
2021-04-13 14:14:29 +0900

    fix: cartPage

--- lib/pages/services/cartPage.dart
+++ lib/pages/services/cartPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:h_order/appRouter.dart';
+import 'package:h_order/components/statusBar.dart';
+import 'package:h_order/constants/customColors.dart';
 import 'package:h_order/models/cartItemModel.dart';
 import 'package:h_order/models/productOptionModel.dart';
 import 'package:intl/intl.dart';
@@ -55,96 +57,142 @@ class _CartPageState extends State<CartPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('상점'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.symmetric(vertical: 12),
-              children: [
-                ...widget.cart
-                        ?.map((cartItem) => _cartItem(cartItem: cartItem)) ??
-                    [],
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                  width: 1,
-                  color: Colors.white10,
+      body: Container(
+        child: Column(
+          children: [
+            StatusBar(),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 24,
                 ),
-              ),
-            ),
-            padding: EdgeInsets.symmetric(
-              horizontal: 12,
-              vertical: 8,
-            ),
-            child: Column(
-              children: [
-                _titleItem(
-                  label: '배달위치',
-                  text: '진주오피스텔 A동 102호',
-                ),
-                _titleItem(
-                  label: '결제정보',
-                  text: '롯데카드 [1234]',
-                  margin: false,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 12),
-                  decoration: BoxDecoration(
-                    border: Border(
-                      top: BorderSide(
-                        width: 1,
-                        color: Colors.white10,
-                      ),
-                    ),
-                  ),
-                ),
-                Row(
+                color: CustomColors.backgroundLightGrey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Text(
-                      '총 금액',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        '${NumberFormat().format(totalAmount)} ₩',
-                        textAlign: TextAlign.right,
-                        style: TextStyle(),
-                      ),
-                    ),
+                    _cartHeader(),
+                    _cartItems(),
+                    _amount(),
+                    _payButton(),
                   ],
                 ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 12),
-            child: FlatButton(
-              color: Colors.blueGrey,
-              onPressed: () {
-                _save();
-              },
-              child: Text(
-                '결제하기',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+
+  _cartHeader() => Row(
+        children: [
+          Text(
+            '장바구니',
+            style: Theme.of(context).textTheme.headline1.copyWith(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          Spacer(),
+          TextButton(
+            onPressed: () {
+              AppRouter.pop();
+            },
+            child: Row(
+              children: [
+                Text('목록보기', style: Theme.of(context).textTheme.bodyText2),
+                Icon(
+                  CupertinoIcons.chevron_right_2,
+                  color: Theme.of(context).textTheme.bodyText2.color,
+                ),
+              ],
+            ),
+          )
+        ],
+      );
+
+  _cartItems() => Expanded(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 12),
+          children: [
+            ...widget.cart?.map(
+                  (cartItem) => _cartItem(cartItem: cartItem),
+                ) ??
+                [],
+          ],
+        ),
+      );
+
+  _paymentInfo() => Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              width: 1,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 8,
+        ),
+        child: Column(
+          children: [
+            _titleItem(
+              label: '배달위치',
+              text: '진주오피스텔 A동 102호',
+            ),
+            _titleItem(
+              label: '결제정보',
+              text: '롯데카드 [1234]',
+              margin: false,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 12),
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    width: 1,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Text(
+                  '총 금액',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    '${NumberFormat().format(totalAmount)} ₩',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+
+  _payButton() => Container(
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: FlatButton(
+          color: Colors.blueGrey,
+          onPressed: () {
+            _save();
+          },
+          child: Text(
+            '결제하기',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ),
+      );
 
   _titleItem({
     String label,
@@ -176,123 +224,178 @@ class _CartPageState extends State<CartPage>
     CartItemModel cartItem,
   }) =>
       Container(
-        margin: EdgeInsets.only(bottom: 12),
-        padding: EdgeInsets.symmetric(horizontal: 12),
-        child: Column(
+        child: Stack(
+          overflow: Overflow.visible,
           children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Column(
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              margin: EdgeInsets.only(bottom: 12),
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        margin: EdgeInsets.only(bottom: 6),
-                        child: Row(
+                      Expanded(
+                        child: Column(
                           children: [
-                            Text(
-                              '${cartItem.name}',
-                              style: TextStyle(),
-                            ),
-                            Expanded(
-                              child: Text(
-                                '${NumberFormat().format(cartItem.totalAmount)} ₩',
-                                textAlign: TextAlign.right,
-                                style: TextStyle(),
+                            Container(
+                              margin: EdgeInsets.only(bottom: 6),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    '${cartItem.name}',
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  // Expanded(
+                                  //   child: Text(
+                                  //     '${NumberFormat().format(cartItem.totalAmount)} ₩',
+                                  //     textAlign: TextAlign.right,
+                                  //     style: TextStyle(color: Colors.black),
+                                  //   ),
+                                  // ),
+                                ],
                               ),
                             ),
+                            _option(
+                              quantity: cartItem.quantity,
+                              name: cartItem.product.name,
+                              price: cartItem.product.price,
+                            ),
+                            ...cartItem.optionQuantity.entries
+                                .where((element) => element.value > 0)
+                                .map((element) {
+                              final option = _optionMap[element.key];
+
+                              return _option(
+                                depth: 1,
+                                quantity: element.value,
+                                name: _optionName(option: option),
+                                price: option.price,
+                              );
+                            }),
                           ],
                         ),
                       ),
-                      _option(
-                        quantity: cartItem.quantity,
-                        name: cartItem.product.name,
-                        price: cartItem.product.price,
-                      ),
-                      ...cartItem.optionQuantity.entries
-                          .where((element) => element.value > 0)
-                          .map((element) {
-                        final option = _optionMap[element.key];
-
-                        return _option(
-                          depth: 1,
-                          quantity: element.value,
-                          name: _optionName(option: option),
-                          price: option.price,
-                        );
-                      }),
                     ],
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 10),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 16,
-                        height: 16,
-                        child: IconButton(
-                          iconSize: 16,
-                          padding: EdgeInsets.zero,
-                          onPressed: () {
-                            if (cartItem.quantity == 1) {
-                              widget.cart.remove(cartItem);
-                            } else {
-                              cartItem.subtract(1);
-                            }
-                            setState(() {});
-                          },
-                          icon: Icon(
-                            CupertinoIcons.minus,
-                            size: 16,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 20,
-                        margin: EdgeInsets.symmetric(horizontal: 12),
-                        child: Text(
-                          '${cartItem.quantity}',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(),
-                        ),
-                      ),
-                      Container(
-                        width: 16,
-                        height: 16,
-                        child: IconButton(
-                          iconSize: 16,
-                          padding: EdgeInsets.zero,
-                          onPressed: () {
-                            cartItem.add(1);
-                            setState(() {});
-                          },
-                          icon: Icon(
-                            CupertinoIcons.plus,
-                            size: 16,
-                          ),
-                        ),
-                      ),
-                    ],
+                  Divider(
+                    color: Colors.black,
+                    height: 30,
+                    thickness: 1,
                   ),
-                ),
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 6),
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    width: 1,
-                    color: Colors.white10,
-                  ),
-                ),
+                  _itemCount(cartItem),
+                ],
               ),
             ),
+            _xmark(cartItem),
           ],
         ),
       );
+
+  _itemCount(CartItemModel cartItem) => Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('수량'),
+          Spacer(),
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _countCalc(cartItem, true),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 12),
+                  child: Text(
+                    '${cartItem.quantity}',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  alignment: Alignment.center,
+                ),
+                _countCalc(cartItem, false),
+              ],
+            ),
+          ),
+        ],
+      );
+
+  _countCalc(CartItemModel cartItem, bool isLeft) => Container(
+        width: 35,
+        height: 35,
+        decoration: BoxDecoration(
+          color: CustomColors.subTextBlack,
+          borderRadius: isLeft
+              ? BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  bottomLeft: Radius.circular(8),
+                )
+              : BorderRadius.only(
+                  topRight: Radius.circular(8),
+                  bottomRight: Radius.circular(8),
+                ),
+        ),
+        child: IconButton(
+          iconSize: 16,
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            if (isLeft) {
+              if (cartItem.quantity == 1) {
+                widget.cart.remove(cartItem);
+              } else {
+                cartItem.subtract(1);
+              }
+            } else {
+              cartItem.add(1);
+            }
+            setState(() {});
+          },
+          icon: Icon(
+            isLeft ? CupertinoIcons.minus : CupertinoIcons.plus,
+            size: 20,
+            color: Theme.of(context).textTheme.bodyText1.color,
+          ),
+        ),
+      );
+
+  _xmark(CartItemModel cartItem) => Positioned(
+        right: 10,
+        top: 10,
+        child: InkWell(
+          onTap: () {
+            widget.cart.remove(cartItem);
+            setState(() {});
+          },
+          child: Icon(
+            CupertinoIcons.xmark,
+            size: 30,
+          ),
+        ),
+      );
+
+  _amount() => Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: EdgeInsets.only(bottom: 12),
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+        child: Row(
+          children: [
+            Text('총 주문금액'),
+            Spacer(),
+            Text('${NumberFormat().format(_amountCalc())}원'),
+            // Text('123'),
+          ],
+        ),
+      );
+
+  _amountCalc() {
+    return widget.cart.map((e) => e.totalAmount).fold(0, (p, c) => p + c);
+  }
 
   _option({
     double depth = 0,
@@ -308,15 +411,15 @@ class _CartPageState extends State<CartPage>
             Text(
               name,
               style: TextStyle(
-                color: Colors.white38,
+                color: Colors.black,
               ),
             ),
             Expanded(
               child: Text(
-                '${NumberFormat().format(price)} ₩',
+                '${NumberFormat().format(price)}원',
                 textAlign: TextAlign.right,
                 style: TextStyle(
-                  color: Colors.white38,
+                  color: Colors.black,
                 ),
               ),
             ),
