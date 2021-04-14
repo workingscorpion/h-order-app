cc49b0250aa5d7a1e80e66d97be13e361574571f
workingscorpion <workingscorpion@globalorder.co.kr>
2021-04-14 12:45:54 +0900

    fix: homeFloatingButton image

--- lib/components/homeFloatingButton.dart
+++ lib/components/homeFloatingButton.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeFloatingButton extends StatelessWidget {
   HomeFloatingButton({this.callback});
@@ -9,15 +10,17 @@ class HomeFloatingButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.width * .12,
-      width: MediaQuery.of(context).size.width * .12,
+      height: MediaQuery.of(context).size.width * .1,
+      width: MediaQuery.of(context).size.width * .1,
+      margin: EdgeInsets.only(bottom: 20),
       child: FloatingActionButton(
         onPressed: () {
           return callback();
         },
-        child: Icon(
-          CupertinoIcons.home,
-          size: 28,
+        child: SvgPicture.asset(
+          'assets/icons/home/home.svg',
+          color: Theme.of(context).primaryColor,
+          height: 60,
         ),
       ),
     );
